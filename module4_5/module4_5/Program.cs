ac8164dbd009cf12f5950c00e740dbf11a0a27e3
mr.god85 <mr.god855@gmail.com>
2019-10-31 23:48:20 +0300

    done

---
+++ module4_5/module4_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        enum Operation
+        {
+            one = 2,
+            two,
+            three = one + two,
+        }
+        public int TotalNumberOfDaysInMonth(int year, int month)
+        {
+            return DateTime.DaysInMonth(year, month);
+        }
+    }
+}
